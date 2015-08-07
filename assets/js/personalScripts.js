76efdd26103f49d016d1070f7f1c2b833a4485fe
aviadhahami <aviadhahami@gmail.com>
2015-08-08 01:24:43 +0300

    Done AB testing Fixed a scrolling bug -> Moved script calls to head

--- assets/js/personalScripts.js
+++ assets/js/personalScripts.js
@@ -2,5 +2,33 @@
  * Created by user on 08/08/15.
  */
 $(document).ready(function(){
-alert(1)
+
+    // ENUM
+    var dict ={
+        1 : '#one',
+        2 :'#two'
+    };
+
+
+    // Picks which div to keep
+    var generateAB = function(){
+      var choosen = randomizeOneOrTwo();
+        //alert(choosen);
+        $(choosen).remove();
+
+        $('#mainScroll').attr('href',getOpposite(choosen));
+        return;
+    };
+
+    var getOpposite = function(picked){
+      return picked === dict[1] ? dict[2] : dict[1];
+    };
+    // Randomizes one or two
+    var randomizeOneOrTwo = function(){
+        return dict[Math.round(Math.random() +1)];
+    }
+
+
+    // Invoke all
+    generateAB();
 });
