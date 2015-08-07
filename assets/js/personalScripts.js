aa5945190f08192e057824de5ee662d11f3fcb38
aviadhahami <aviadhahami@gmail.com>
2015-08-08 02:06:53 +0300

    Form modifivations

--- assets/js/personalScripts.js
+++ assets/js/personalScripts.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
     };
 
     var IDToNumber = {
-'#one' : 1,
+        '#one' : 1,
         '#two' : 2
     };
 
 
     // Picks which div to keep
     var generateAB = function(){
-      var choosen = randomizeOneOrTwo();
+        var choosen = randomizeOneOrTwo();
         //alert(choosen);
         $(choosen).remove();
 
@@ -26,12 +26,20 @@ $(document).ready(function(){
     };
 
     var getOpposite = function(picked){
-      return picked === numberToID[1] ? numberToID[2] : numberToID[1];
+        return picked === numberToID[1] ? numberToID[2] : numberToID[1];
     };
     // Randomizes one or two
     var randomizeOneOrTwo = function(){
         return numberToID[Math.round(Math.random() +1)];
     }
+
+    // Listeners
+    $('#submit').click(function(){
+        var userInput = $('#answer').val();
+        // TODO: Ajax call to server with answer
+        $('#form').html('<p>Thank you for your answer! <br/>' +
+            ' A mail will be sent soon!</p>');
+    });
 
 
     // Invoke all
