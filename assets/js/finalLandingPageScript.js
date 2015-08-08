f6a89c5ec6268dd402c4bbf06425544bdd2248c2
aviadhahami <aviadhahami@gmail.com>
2015-08-08 16:07:36 +0300

    Done implementing write to file

--- assets/js/finalLandingPageScript.js
+++ assets/js/finalLandingPageScript.js
@@ -2,5 +2,18 @@
  * Created by user on 08/08/15.
  */
 $(document).ready(function(){
-    alert(1);
+    $('#submit').click(function(){
+
+        var userMail = $('#userMail').val().replace(/\s/g, '');
+        if (userMail.length > 0 ){
+            var serverFileLocation = 'server/process.php';
+            var queryString = '?mail='  + userMail;
+            var URI = serverFileLocation + queryString;
+            $.get( URI, function( data ) {
+                console.log(data);
+            });
+        }
+
+    });
+
 });
--- server/process.php
+++ server/process.php
@@ -1,4 +1,19 @@
 <?php
-    echo 1+1;
+
+$email = $_GET['mail'];
+
+if($email){
+$file = 'db.txt';
+// Open the file to get existing content
+$current = file_get_contents($file);
+// Append a new person to the file
+
+//echo $email;
+$current .= $email . ' | ';
+// Write the contents back to the file
+file_put_contents($file, $current);
+
+echo 'OK';
+}else{echo 'BAD';}
 
 ?>
